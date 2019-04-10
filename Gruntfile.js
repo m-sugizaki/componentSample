e6566b5cb3eb9b062dc80e2f3740230461a27d76
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-04-10 13:54:22 +0900

    1354

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ grunt.loadNpmTasks('grunt-war');
       
   options: {
 //          war_dist_folder: 'C:/workspace/war', // warファイル生成先フォルダ
-          war_dist_folder: 'temp/', // warファイル生成先フォルダ
+          war_dist_folder: 'temp2/', // warファイル生成先フォルダ
           war_name: 'componentSample' // warファイル名
         },
         files: [
