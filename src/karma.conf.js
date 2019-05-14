78b48b45c8c244f151ad51d4766b135ec09293ee
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-05-14 17:47:19 +0900

    karma

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,20 +10,47 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-spec-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-typescript-preprocessor'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    // karma-typescript-preprocessor
+    preprocessors: {
+      '**/*.ts': ['typescript']
+    },
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options: {
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: true, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.ts$/, '.js');
+      }
+    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/componentSample'),
+      dir: require('path').join(__dirname, '../report/coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    // reporters: ['progress', 'kjhtml'], // karma-spec-reporter使用
+    reporters: ['spec', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: '../report/unit_test'
+    },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['Chrome'],
     // karma for trvis
