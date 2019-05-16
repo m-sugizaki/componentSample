3e7ea9effbe7a37aa14932ce6e23b5b0bc6a3f23
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-05-16 14:31:42 +0900

    protractor

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,13 +11,13 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--test-type=webdriver"
+      args: [ "--headless", "--disable-gpu", "--no-sandbox"
       // "--window-size=800,600" 
       ]
     } 
   },
   directConnect: true,
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
