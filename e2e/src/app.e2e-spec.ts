5bc288c08e29bb145de5d8bb5bbb05134ace2ef2
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-05-16 11:49:54 +0900

    protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,17 +1,28 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('componentSample App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('ページタイトル', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to componentSample!');
+    expect(page.getTitleText()).toEqual('ComponentSample');
+//    expect(page.getTitleText()).toEqual('Welcome to componentSample!');
   });
+  it('初期ページの「title」要素', () => {
+    expect(page.getTitleText2()).toEqual('メンバー一覧');
+  });
+
+  it('「メンバーを追加する」に遷移', () => {
+    page.navigateTo();
+    expect(page.openNewMember()).toEqual('メンバー作成');
+  });
+
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -21,3 +32,4 @@ describe('workspace-project App', () => {
     } as logging.Entry));
   });
 });
+
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,22 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    // ページのタイトルを取得
+    return browser.getTitle() as Promise<string>;
+    // return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+  getTitleText2() {
+    // スタイルクラスを指定して取得
+    // <div class="title">メンバー一覧</div>
+    return element(by.className('title')).getText() as Promise<string>;
+  }
+
+  openNewMember() {
+    // スタイルの要素を指定して取得
+    // cssファイルに書かれた要素
+    element(by.css('.button-wide ')).click();
+    // return browser.getTitle() as Promise<string>;
+    return element(by.className('title')).getText() as Promise<string>;
+  }
+
 }
