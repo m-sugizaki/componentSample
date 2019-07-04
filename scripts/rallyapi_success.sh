f9deec02bdc8118dcc975cec2d1507da3fae4cae
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-07-04 17:01:03 +0900

    US557

---
+++ scripts/rallyapi_success.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Update Rally"
+
+CURDIR=`pwd`
+
+US=`git log -1 | grep US`
+echo ${US}
+
+export TZ="Asia/Tokyo"
+DATESTR=`date +%Y%m%d %H%M`
+echo ${DATESTR}
+
+TXT="componentSample job SUCCESS!!! (${DATESTR})"
+echo ${TXT}
+
+ACT=2
+echo ${ACT}
+
+cd scripts/AgileAPIScript_Run
+pwd
+
+cd ${CURDIR}
+pwd
