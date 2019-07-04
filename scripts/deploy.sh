986bbc46990f8b42e162860f81eb897c023969d3
Mutsumi Sugizaki <m-sugizaki@cubesystem.co.jp>
2019-07-04 17:55:52 +0900

    US557

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -74,7 +74,7 @@ echo "Update Rally"
 cd ${CURDIR}
 cd scripts/AgileAPIScript_Run
 chmod 555 ./agileApiObjectHandleBash.sh 
-./agileApiObjectHandleBash.sh ${ACT} ${US} ${TXT}
+./agileApiObjectHandleBash.sh ${ACT} ${US} "${TXT}"
 
 cd ${CURDIR}
 pwd
